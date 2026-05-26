836c50b1d125516012de0031f5f548e15cfc3b2c
basharmansour-it-eng <basharman2003@gmail.com>
2026-05-26 15:48:45 +0300

    edit the main

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      title: 'Stock Mate',
       locale: Get.deviceLocale,
       debugShowCheckedModeBanner: false,
 
